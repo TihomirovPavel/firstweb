7b3f1013eb0cda6dce646fc975dd56e8da421e3a
P.Tihomirov <pavel.tihomirov@ctco.lv>
2018-08-14 10:12:50 +0300

    Empty commit to check if the comit author changed

--- src/main/java/lv/ctco/javaschool/firstweb/HelloWorldServlet.java
+++ src/main/java/lv/ctco/javaschool/firstweb/HelloWorldServlet.java
@@ -19,6 +19,8 @@ public class HelloWorldServlet extends HttpServlet {
                 out.append("You are logged in, " + username);
             } else {
                 out.append("Wrong, Password");
+
+
             }
         }
     }
